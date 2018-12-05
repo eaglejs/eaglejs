531b5fe55d25cc7691ccc59be71f628d4a6d84e6
eaglejs <joshuaseagle@gmail.com>
2018-12-04 20:51:20 -0500

    splitting up css and added assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SocialCodingComponent } from './social-coding/social-coding.component';
 import { AchievementsComponent } from './achievements/achievements.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
     PortfolioComponent,
     SocialCodingComponent,
     AchievementsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
+    AngularFontAwesomeModule,
     AppRoutingModule,
     BrowserModule
   ],
--- src/app/social-coding/social-coding.component.ts
+++ src/app/social-coding/social-coding.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social-coding',
   templateUrl: './social-coding.component.html',
-  styleUrls: ['./social-coding.component.css']
+  styleUrls: ['./social-coding.component.scss']
 })
 export class SocialCodingComponent implements OnInit {
 
